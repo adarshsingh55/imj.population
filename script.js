ecc71351a510f94fae16914c90aacffd14f0cc1b
Adarsh singh <97974577+adarshsingh55@users.noreply.github.com>
2022-05-07 09:07:54 +0530

    2 bug fixed

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var arr = [  'neko' , ' shinobu' , 'megumin' , 'bully' , 'cuddle' , 'cry' , 'hug' , 'awoo' , 'kiss' , 'lick' , 'pat' , 'smug' , 'bonk' , 'yeet' , 'blush' , 'smile' , 'wave' , 'highfive' , 'handhold' , 'nom' , 'bite' , 'glomp' , 'slap' , 'kill' , 'kick' , 'happy' , 'wink' , 'poke' , 'dance' , ' cringe ']
+var arr = [  'neko' , 'shinobu' , 'megumin' , 'bully' , 'cuddle' , 'cry' , 'hug' , 'awoo' , 'kiss' , 'lick' , 'pat' , 'smug' , 'bonk' , 'yeet' , 'blush' , 'smile' , 'wave' , 'highfive' , 'handhold' , 'nom' , 'bite' , 'glomp' , 'slap' , 'kill' , 'kick' , 'happy' , 'wink' , 'poke' , 'dance' , 'cringe']
 let arlength = arr.length
 console.log(arlength)
 let slect = document.getElementById("car") ;
